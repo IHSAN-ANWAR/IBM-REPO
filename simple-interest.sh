8797bd9408db31ac3ab48999b317869fb944d05d
IHSAN ANWAR <ihsan.anwar4321@gmail.com>
2025-11-19 12:27:11 +0500

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Simple Interest Calculator
+
+# simple interest = (principal * rate * time) / 100
+
+principal=$1
+rate=$2
+time=$3
+
+interest=$((principal * rate * time / 100))
+
+echo "The simple interest is: $interest"
 
